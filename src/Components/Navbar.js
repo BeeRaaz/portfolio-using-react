6d2b5f4719eea4204074433df684156e3b3637ad
BeeRaaz <137605748+BeeRaaz@users.noreply.github.com>
2023-11-30 14:03:26 +0545

    Navigation Bar is completed.

---
+++ src/Components/Navbar.js
@@ -0,0 +1,22 @@
+import { hover } from '@testing-library/user-event/dist/hover'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav className="flex justify-between py-10 px-40 bg-gray-100 h-1/5">
+        <div id="navLeft">
+            <h1 className="text-4xl cursor-pointer">Biraj Dulal</h1>
+        </div>
+        <div id="navRight" className="flex gap-14">
+            <a href=""><h3 className="text-xl hover:text-gray-500">Home</h3></a>
+            <a href=""><h3 className="text-xl hover:text-gray-500">About</h3></a>
+            <a href=""><h3 className="text-xl hover:text-gray-500">Works</h3></a>
+            <a href=""><h3 className="text-xl hover:text-gray-500">Contact</h3></a>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
